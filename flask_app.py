6b8634d891038fdd2ae003e5e753c18dadf564a7
EgliMNG <luca.egli@mng.ch>
2025-12-19 14:13:36 +0000

    Fix

--- flask_app.py
+++ flask_app.py
@@ -121,11 +121,11 @@ def index():
         todos = db_read("SELECT id, content, due FROM todos WHERE user_id=%s ORDER BY due", (current_user.id,))
         return render_template("main_page.html", todos=todos)
 
-# POST
-content = request.form["contents"]
-due = request.form["due_at"]
-db_write("INSERT INTO todos (user_id, content, due) VALUES (%s, %s, %s)", (current_user.id, content, due, ))
-return redirect(url_for("index"))
+    # POST
+    content = request.form["contents"]
+    due = request.form["due_at"]
+    db_write("INSERT INTO todos (user_id, content, due) VALUES (%s, %s, %s)", (current_user.id, content, due, ))
+    return redirect(url_for("index"))
 
 @app.post("/complete")
 @login_required
